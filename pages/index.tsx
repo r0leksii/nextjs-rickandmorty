52fed4f5181c6e87d509ae19d648bb82bdefa17d
r0leksii <rozhkoaleks@gmail.com>
2024-05-26 19:11:19 +0300

    rewrite code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
+import { useEffect } from 'react'
+
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import { getLayout } from '../components/Layout/BaseLayout/BaseLayout'
 import { PageWrapper } from '../components/PageWrapper/PageWrapper'
 import { NextPageWithLayout } from './_app'
 
-const Home: NextPageWithLayout = () => (
-  <PageWrapper>
-    <Image alt={'Next.js Logo'} height={37} priority src={'/next.svg'} width={180} />
-  </PageWrapper>
-)
+const Home: NextPageWithLayout = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/characters').then(r => r)
+  })
+
+  return (
+    <PageWrapper>
+      <Image alt={'Next.js Logo'} height={37} priority src={'/next.svg'} width={180} />
+    </PageWrapper>
+  )
+}
 
 Home.getLayout = getLayout
 
